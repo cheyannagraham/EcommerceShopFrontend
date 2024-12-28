422a9db9d3818539df97b2bcbc662f51d2485a28
chey <cheyanna.graham@gmail.com>
2024-12-27 17:59:36 -0800

    add shopping cart icon

--- src/app/Components/cart-status/cart-status.component.ts
+++ src/app/Components/cart-status/cart-status.component.ts
@@ -1,18 +1,20 @@
 import {Component} from '@angular/core';
 import {CartService} from '../../Services/cart.service';
 import {CurrencyPipe} from '@angular/common';
+import {faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+
 
 @Component({
   selector: 'app-cart-status',
-  imports: [
-    CurrencyPipe
-  ],
+  imports: [CurrencyPipe, FaIconComponent],
   templateUrl: './cart-status.component.html',
   styleUrl: './cart-status.component.css'
 })
 export class CartStatusComponent {
   cartPrice = 0;
   cartQuantity = 0;
+
 
   constructor(public cartService: CartService) {
   }
@@ -32,4 +34,5 @@ export class CartStatusComponent {
   }
 
 
+  protected readonly faShoppingCart = faShoppingCart;
 }
