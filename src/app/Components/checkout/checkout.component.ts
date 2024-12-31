0eba0ec32dc43d11c3d97d56ee41bc280b2d4242
chey <cheyanna.graham@gmail.com>
2024-12-31 10:08:53 -0800

    add customer, shipping, billing, form fields

--- src/app/Components/checkout/checkout.component.ts
+++ src/app/Components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {Component} from '@angular/core';
+import {Form, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -8,5 +8,61 @@ import {ReactiveFormsModule} from '@angular/forms';
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  checkoutForm: any;
+  billingSameAsShipping: FormControl;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.billingSameAsShipping = new FormControl("");
+  }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.checkoutForm = this.formBuilder.group({
+
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+
+      shippingAddress: this.formBuilder.group({
+        country: [''],
+        streetAddress: [''],
+        city: [''],
+        state: [''],
+        zipCode: ['']
+      }),
+
+      billingAddress: this.formBuilder.group({
+        country: [''],
+        streetAddress: [''],
+        city: [''],
+        state: [''],
+        zipCode: ['']
+      }),
+
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        cardholderName: [''],
+        cardNumber: [''],
+        secCode: [''],
+        expMonth: [''],
+        expYear: [''],
+      })
+    })
+  }
+
+  submitForm() {
+    console.log(this.checkoutForm.value);
+  }
+
+  setBillingSameAsShipping() {
+    this.billingSameAsShipping.value ?
+      this.checkoutForm.patchValue({billingAddress: this.checkoutForm.value.shippingAddress}) :
+      this.checkoutForm.get("billingAddress").reset();
+  }
 
 }
