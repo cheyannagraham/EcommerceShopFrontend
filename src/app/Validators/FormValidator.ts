50502307b4104e76a74c4ab3e5ae1a9c176582f1
chey <cheyanna.graham@gmail.com>
2025-01-06 09:28:10 -0800

    fix minlength bug in custom validator

--- src/app/Validators/FormValidator.ts
+++ src/app/Validators/FormValidator.ts
@@ -2,8 +2,7 @@ import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
 export function noWhitespace(): ValidatorFn {
   return (control:AbstractControl): ValidationErrors | null => {
-    const value = control.value
-    return value.trim().length > 0 ? null : {noWhitespace: true};
+    return control.value.trim().length > 1 ? null : {noWhitespace: true};
   }
 
 }
