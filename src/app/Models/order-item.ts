8ac6a8e4c81c93827fa76a295348729debd4a43a
chey <cheyanna.graham@gmail.com>
2025-01-10 14:05:23 -0800

    create checkout models

---
+++ src/app/Models/order-item.ts
@@ -0,0 +1,7 @@
+export class OrderItem {
+
+  constructor(public imageUrl:string,
+              public unitPrice: number,
+              public prodcutId: number,
+              public quantity: number){}
+}
