5d3c2136e9d86b2357613cff7d3cc0c488079365
chey <cheyanna.graham@gmail.com>
2025-01-18 21:35:14 -0800

    add node server

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + process.env.STATIC));
+
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + process.env.INDEX);
+});
+
+app.listen(process.env.PORT || 4200, () => {
+  console.log('Server started..........');
+});
